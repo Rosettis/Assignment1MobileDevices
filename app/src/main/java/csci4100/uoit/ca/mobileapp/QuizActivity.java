968946a01a461c285c4428fc9600bc7559c6471f
Matthew <matt_rose21@hotmail.com>
2015-10-21 19:19:33 -0400

    Finalization of the app, including ability for returning after the questions are done

--- app/src/main/java/csci4100/uoit/ca/mobileapp/QuizActivity.java
+++ app/src/main/java/csci4100/uoit/ca/mobileapp/QuizActivity.java
@@ -4,21 +4,34 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class QuizActivity extends Activity {
+    //Global Variables
     String[] questionArray;
+    int yesCount;
+    int noCount;
+    int size;
+    String[] check;
+    int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
         questionArray = getResources().getStringArray(R.array.quiz_question);
+        size = questionArray.length;
+        check = new String[size];
+        for (int j = 0; j < size; j++){
+            check[j] = "no";
+        }
     }
     @Override
     public void onResume() {
@@ -28,35 +41,53 @@ public class QuizActivity extends Activity {
     private void updateTextView() {
         TextView textView = (TextView)findViewById(R.id.randomTextView);
         Random random = new Random();
-
-        int maxIndex = questionArray.length;
-        int generatedIndex = random.nextInt(maxIndex);
+        int generatedIndex = random.nextInt(size);
+        String checkString = "asked";
+        int count = 0;
+        while(true) {
+            Log.d("preTest", Integer.toString(count));
+            count = 0;
+            //finding if all questions have been asked
+            for (i = 0; i < size; i++) {
+                Log.d("trust",check[i]);
+                if (check[i].equals(checkString)) {
+                    count++;
+                }
+            }//Exiting if all questions asked
+            if (count == size) {
+                break;
+            }//Checking if question has already been asked
+            if (check[generatedIndex].equals(checkString)) {
+                Log.d("question already asked", Integer.toString(generatedIndex+1));
+            }//Checking if its a new question
+            else {
+                check[generatedIndex] = checkString;
+                Log.d("question number", Integer.toString(generatedIndex+1));
+                break;
+            }
+            generatedIndex = random.nextInt(size);
+        }
+        if (count == size) {
+            switchBack(this.findViewById(android.R.id.content));
+        }
+        Log.d("worked",Integer.toString(count));
         textView.setTextSize(40);
         textView.setTextColor(Color.WHITE);
         textView.setText(questionArray[generatedIndex]);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     public void switchBack(View view) {
         Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        intent.putExtra("yes",yesCount);
+        intent.putExtra("no", noCount);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+    public void pressYes(View view){
+        yesCount++;
+        updateTextView();
+    }
+    public void pressNo(View view){
+        noCount++;
+        updateTextView();
     }
 }
